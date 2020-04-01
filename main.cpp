6fcd805494ea8558d7bc6a1bcc68ffa94d6bbca7
YuChengHan <chenghan03131@gapp.nthu.edu.tw>
2020-04-01 17:33:02 +0800

    pwm sweep

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include "mbed.h"
 #include "uLCD_4DGL.h"
+PwmOut PWM1(D6);
 
-uLCD_4DGL scr(D1, D0, D2);
-//PwmOut pwm(D0);
-int main()
-{
-      //scr.background_color(WHITE);
-      //scr.textbackground_color(WHITE);
-      //scr.color(BLUE);
-      scr.cls();
+int main() {
+    /*scr.cls();
       scr.text_width(2);
       scr.text_height(2);
       scr.printf("107033131");
-      //scr.color(GREEN);
       scr.line(20,20,50,20,WHITE);
       scr.line(20,20,20,50,WHITE);
       scr.line(20,50,50,50,WHITE);
-      scr.line(50,20,50,50,WHITE);
-      //pwm.period(0.001);
-      //pwm=0.5;
-
+      scr.line(50,20,50,50,WHITE);*/
+  PWM1.period(0.001);
+  for(float i=0;i<1;i+=0.1){
+      PWM1=i;
+  }for(float i=1;i>=0;i-=0.1){
+      PWM1=i;
+  }
+  
 }
