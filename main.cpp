a4e00eac0f6ca3d28f50b7f330a130572ef785b6
YuChengHan <chenghan03131@gapp.nthu.edu.tw>
2020-04-01 17:09:32 +0800

    uLCD

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL scr(D1, D0, D2);
-PwmOut pwm(D0);
+//PwmOut pwm(D0);
 int main()
 {
       //scr.background_color(WHITE);
@@ -13,11 +13,11 @@ int main()
       scr.text_height(2);
       scr.printf("107033131");
       //scr.color(GREEN);
-      scr.line(20,20,40,10,WHITE);
-      scr.line(20,20,20,40,WHITE);
-      scr.line(20,40,40,40,WHITE);
-      scr.line(40,20,40,40,WHITE);
-      pwm.period(0.001);
-      pwm=1;
+      scr.line(20,20,50,20,WHITE);
+      scr.line(20,20,20,50,WHITE);
+      scr.line(20,50,50,50,WHITE);
+      scr.line(50,20,50,50,WHITE);
+      //pwm.period(0.001);
+      //pwm=0.5;
 
 }
